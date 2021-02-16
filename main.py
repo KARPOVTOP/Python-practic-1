149d3f3386fccfa6e71afde23def6cf441c1b732
Mich <coolxex86@gmail.com>
2021-02-16 16:01:21 +0300

    edit2

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #Вариант 5
 #Задание 1.1
 import math
-
+import cmath
 
 def function1(x):
     return (x**4 + 68*x**5)/(math.cos(x) - math.e**x - 67) - (45*x**5 + x**3)/(x**7+(x/25)) - (x**6 - x**2 - 95)
 
+print("Задание 1.1")
 print("1)"f'{function1(-2):.2e}')
 print("2)"f'{function1(-11):.2e}')
+print()
 
 #Задание 1.2
 
@@ -19,30 +21,36 @@ def function2(x):
     if (x >= 99):
         return 92*x**3 + x**7 - 20
 
+print("Задание 1.2")
 print("1)"f'{function2(77):.2e}')
 print("2)"f'{function2(48):.2e}')
+print()
 
 #Задание 1.3
 
 def function3(n):
     x = 0
     y = 0
+    for i in range(1, n + 1):
+        x = x + (i + math.log(i))
     for i in range(n + 1):
-        x = x + (i**6 + math.sin(i) - 14)
-    for i in range(n + 1):
-        y = y + (i**4 - math.sin(i))
-    return x + y
+        y = y + (i**5 + i**2)
+    return 95 * x + y
 
-print("1)"f'{function3(69):.2e}')
-print("2)"f'{function3(43):.2e}')
+print("Задание 1.3")
+print("1)"f'{function3(27):.2e}')
+print("2)"f'{function3(11):.2e}')
+print()
 
 #Задание 1.4
 
 def function4(n):
     if n == 0:
-        return 4
+        return 2
     else:
-        return math.cos(function4(n - 1)) - 1/96 * function4(n - 1)**3
+        return 1/22 * (function4(n - 1)**2) - 1/76 * function4(n - 1)
 
-print("1)"f'{function4(14):.2e}')
-print("2)"f'{function4(10):.2e}')
+print("Задание 1.4")
+print("1)"f'{function4(3):.2e}')
+print("2)"f'{function4(16):.2e}')
+print()
