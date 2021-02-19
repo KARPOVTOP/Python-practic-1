c07ac2e61b7f6757c713f89b34cae727174b6b0b
Mich <coolxex86@gmail.com>
2021-02-19 21:49:05 +0300

    edit4

--- main.py
+++ main.py
@@ -3,17 +3,17 @@
 import math
 import cmath
 
-def function1(x):
+def f1(x):
     return (x**4 + 68*x**5)/(math.cos(x) - math.exp(x) - 67) - (45*x**5 + x**3)/(x**7+(x/25)) - (x**6 - x**2 - 95)
 
 print("Задание 1.1")
-print("1)"f'{function1(-2):.2e}')
-print("2)"f'{function1(-11):.2e}')
+print("1)"f'{f1(-2):.2e}')
+print("2)"f'{f1(-11):.2e}')
 print()
 
 #Задание 1.2
 
-def function2(x):
+def f2(x):
     if (x < 70):
         return x + 59*x**6
     if (70 <= x < 99):
@@ -22,13 +22,13 @@ def function2(x):
         return 92*x**3 + x**7 - 20
 
 print("Задание 1.2")
-print("1)"f'{function2(77):.2e}')
-print("2)"f'{function2(48):.2e}')
+print("1)"f'{f2(77):.2e}')
+print("2)"f'{f2(48):.2e}')
 print()
 
 #Задание 1.3
 
-def function3(n):
+def f3(n):
     x = 0
     y = 0
     for i in range(1, n + 1):
@@ -38,19 +38,19 @@ def function3(n):
     return 95 * x + y
 
 print("Задание 1.3")
-print("1)"f'{function3(27):.2e}')
-print("2)"f'{function3(11):.2e}')
+print("1)"f'{f3(27):.2e}')
+print("2)"f'{f3(11):.2e}')
 print()
 
 #Задание 1.4
 
-def function4(n):
+def f4(n):
     if n == 0:
         return 2
     else:
-        return 1/22 * (function4(n - 1)**2) - 1/76 * function4(n - 1)
+        return 1/22 * (f4(n - 1)**2) - 1/76 * f4(n - 1)
 
 print("Задание 1.4")
-print("1)"f'{function4(3):.2e}')
-print("2)"f'{function4(16):.2e}')
+print("1)"f'{f4(3):.2e}')
+print("2)"f'{f4(16):.2e}')
 print()
